93a4d63e0fa91f8b504c0ceb339439b4c4deb8ea
Austin Willis <austinwillis8@gmail.com>
2016-06-27 22:55:30 -0500

    need to figure out how to loop json in angular template

--- client/app/ts/buttonlist.component.ts
+++ client/app/ts/buttonlist.component.ts
@@ -1,4 +1,6 @@
 import {Component} from 'angular2/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'buttonlist',
@@ -6,4 +8,21 @@ import {Component} from 'angular2/core';
 })
 
 export class ButtonList {
+  public buttons;
+
+  constructor(private http: Http) {}
+
+  ngOnInit() {
+    this.getButtons();
+  }
+
+  getButtons() {
+    this.http.get('http://localhost:3000/api/buttons')
+      .map((res.Response) => res.json())
+      .subscribe(
+        data => { this.buttons = data },
+        err => console.error(err),
+        () => console.log('done')
+      );
+  }
 }
